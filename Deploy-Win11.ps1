ba14ee456eef80b31488505f05c293ff7c4dd3f3
Michael Hanson <15201245+mikedhanson@users.noreply.github.com>
2025-09-08 14:10:40 -0500

    Create Deploy-Win11.ps1

---
+++ Deploy-Win11.ps1
@@ -0,0 +1,67 @@
+#Variables to define the Windows OS / Edition etc to be applied during OSDCloud
+$Product = (Get-MyComputerProduct)
+$Model = (Get-MyComputerModel)
+$Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
+$OSVersion = 'Windows 11' #Used to Determine Driver Pack
+$OSReleaseID = '24H2' #Used to Determine Driver Pack
+$OSName = 'Windows 11 24H2 x64'
+$OSEdition = 'Enterprise'
+$OSActivation = 'Volume'
+$OSLanguage = 'en-us'
+
+
+Used to Determine Driver Pack
+$DriverPack = Get-OSDCloudDriverPack -Product $Product -OSVersion $OSVersion -OSReleaseID $OSReleaseID
+
+if ($DriverPack){
+    $Global:MyOSDCloud.DriverPackName = $DriverPack.Name
+}
+
+#Set OSDCloud Vars
+$Global:MyOSDCloud = [ordered]@{
+    Restart = [bool]$False
+    RecoveryPartition = [bool]$true
+    OEMActivation = [bool]$True
+    WindowsUpdate = [bool]$true
+    WindowsUpdateDrivers = [bool]$true
+    WindowsDefenderUpdate = [bool]$true
+    SetTimeZone = [bool]$true
+    ClearDiskConfirm = [bool]$False
+    ShutdownSetupComplete = [bool]$false
+    SyncMSUpCatDriverUSB = [bool]$true
+    CheckSHA1 = [bool]$true
+}
+
+# cmdlet from osdCloud
+if (Test-HPIASupport){
+    Write-Host "Detected HP Device, Enabling HPIA, HP BIOS and HP TPM Updates"
+    #$Global:MyOSDCloud.DevMode = [bool]$True
+    $Global:MyOSDCloud.HPTPMUpdate = [bool]$True
+    if ($Product -ne '83B2' -and $Model -notmatch "zbook"){
+        $Global:MyOSDCloud.HPIAALL = [bool]$true
+    }
+
+    #{$Global:MyOSDCloud.HPIAALL = [bool]$true}
+
+    $Global:MyOSDCloud.HPBIOSUpdate = [bool]$true
+
+    #$Global:MyOSDCloud.HPCMSLDriverPackLatest = [bool]$true #In Test 
+    #Set HP BIOS Settings to what I want:
+
+    Write-Host "Setting HP BIOS Settings"
+
+    #iex (irm https://raw.githubuserconten.tcom/gwblok/garytown/master/OSD/CloudOSD/Manage-HPBiosSettings.ps1)
+    #Manage-HPBiosSettings -SetSettings
+
+}
+
+
+#Launch OSDCloud
+Write-Host "Starting OSDCloud" -ForegroundColor Green
+write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
+
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+
+write-host "OSDCloud Complete" -ForegroundColor Green
+
+#Reboot?

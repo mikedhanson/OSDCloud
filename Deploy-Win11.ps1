46725d1bfab3ab6a40e4e10f7901903912779093
Michael Hanson <15201245+mikedhanson@users.noreply.github.com>
2025-09-08 15:46:44 -0500

    Update Deploy-Win11.ps1

--- Deploy-Win11.ps1
+++ Deploy-Win11.ps1
@@ -1,34 +1,58 @@
+<#PSScriptInfo
+.VERSION 23.6.10.1
+.GUID 9670c013-d1b1-4f5d-9bd0-0fa185b9f203
+.AUTHOR David Segura @SeguraOSD
+.COMPANYNAME osdcloud.com
+.COPYRIGHT (c) 2023 David Segura osdcloud.com. All rights reserved.
+.TAGS OSDeploy OSDCloud WinPE OOBE Windows AutoPilot
+.LICENSEURI 
+.PROJECTURI https://github.com/OSDeploy/OSD
+.ICONURI 
+.EXTERNALMODULEDEPENDENCIES 
+.REQUIREDSCRIPTS 
+.EXTERNALSCRIPTDEPENDENCIES 
+.RELEASENOTES
+Script should be executed in a Command Prompt using the following command
+powershell Invoke-Expression -Command (Invoke-RestMethod -Uri sandbox.osdcloud.com)
+This is abbreviated as
+powershell iex (irm sandbox.osdcloud.com)
+#>
+#Requires -RunAsAdministrator
+<#
+.SYNOPSIS
+    PowerShell Script which supports the OSDCloud environment
+.DESCRIPTION
+    PowerShell Script which supports the OSDCloud environment
+.NOTES
+    Version 23.6.10.1
+.LINK
+    https://raw.githubusercontent.com/OSDeploy/OSD/master/cloud/sandbox.osdcloud.com.ps1
+.EXAMPLE
+    powershell iex (irm sandbox.osdcloud.com)
+#>
 [CmdletBinding()]
-param(
-    [string]$OSName       = "Windows 11 24H2 x64",
-    [string]$OSEdition    = "Pro",
-    [string]$OSActivation = "Retail",
-    [string]$OSLanguage   = "en-us"
-)
-
-$ScriptName = 'Michaelhanson.dev'
-$ScriptVersion = '1.0.2'
-
-write-host "os name: $OSName"
-
-read-host "Brfeak here" 
+param()
+$ScriptName = 'sandbox.osdcloud.com'
+$ScriptVersion = '23.6.10.1'
 
 #region Initialize
 $Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-$ScriptName.log"
-$null = Start-Transcript -Path (Join-Path "$env:SystemRoot\Temp" $Transcript) #-ErrorAction Ignore
+$null = Start-Transcript -Path (Join-Path "$env:SystemRoot\Temp" $Transcript) -ErrorAction Ignore
 
 if ($env:SystemDrive -eq 'X:') {
     $WindowsPhase = 'WinPE'
 }
 else {
     $ImageState = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\State' -ErrorAction Ignore).ImageState
-    if ($env:UserName -eq 'defaultuser0') { $WindowsPhase = 'OOBE' }
-    elseif ($ImageState -eq 'IMAGE_STATE_SPECIALIZE_RESEAL_TO_OOBE') { $WindowsPhase = 'Specialize' }
-    elseif ($ImageState -eq 'IMAGE_STATE_SPECIALIZE_RESEAL_TO_AUDIT') { $WindowsPhase = 'AuditMode' }
-    else { $WindowsPhase = 'Windows' }
+    if ($env:UserName -eq 'defaultuser0') {$WindowsPhase = 'OOBE'}
+    elseif ($ImageState -eq 'IMAGE_STATE_SPECIALIZE_RESEAL_TO_OOBE') {$WindowsPhase = 'Specialize'}
+    elseif ($ImageState -eq 'IMAGE_STATE_SPECIALIZE_RESEAL_TO_AUDIT') {$WindowsPhase = 'AuditMode'}
+    else {$WindowsPhase = 'Windows'}
 }
 
 Write-Host -ForegroundColor Green "[+] $ScriptName $ScriptVersion ($WindowsPhase Phase)"
+Invoke-Expression -Command (Invoke-RestMethod -Uri functions.osdcloud.com)
+#endregion
 
 #region Admin Elevation
 $whoiam = [system.security.principal.windowsidentity]::getcurrent().name
@@ -42,66 +66,46 @@ else {
 }
 #endregion
 
-#region TLS
-Write-Host -ForegroundColor Green "[+] Enabling TLS 1.2"
+#region Transport Layer Security (TLS) 1.2
+Write-Host -ForegroundColor Green "[+] Transport Layer Security (TLS) 1.2"
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 #endregion
 
 #region WinPE
 if ($WindowsPhase -eq 'WinPE') {
-    Invoke-Expression -Command (Invoke-RestMethod -Uri functions.osdcloud.com)
-    #osdcloud-StartWinPE -OSDCloud
-
+    #Process OSDCloud startup and load Azure KeyVault dependencies
+    osdcloud-StartWinPE -OSDCloud -KeyVault
     Write-Host -ForegroundColor Cyan "To start a new PowerShell session, type 'start powershell' and press enter"
     Write-Host -ForegroundColor Cyan "Start-OSDCloud, Start-OSDCloudGUI, or Start-OSDCloudAzure, can be run in the new PowerShell window"
-
-    # Global Vars for OSDCloud
-    $Global:MyOSDCloud = [ordered]@{
-        Restart               = $false
-        RecoveryPartition     = $true
-        OEMActivation         = $true
-        WindowsUpdate         = $true
-        WindowsUpdateDrivers  = $true
-        WindowsDefenderUpdate = $true
-        SetTimeZone           = $true
-        ClearDiskConfirm      = $false
-        ShutdownSetupComplete = $false
-        SyncMSUpCatDriverUSB  = $true
-        CheckSHA1             = $true
-    }
-
-    if (Test-HPIASupport) {
-        Write-Host "Detected HP Device, enabling HPIA + BIOS/TPM updates"
-        $Global:MyOSDCloud.HPTPMUpdate = $true
-        $Global:MyOSDCloud.HPIAALL     = $true
-        $Global:MyOSDCloud.HPBIOSUpdate = $true
-    }
-
-    Write-Host "Starting OSDCloud with:" -ForegroundColor Green
-    Write-Host "   OSName: $OSName"
-    Write-Host "   OSEdition: $OSEdition"
-    Write-Host "   OSActivation: $OSActivation"
-    Write-Host "   OSLanguage: $OSLanguage"
-
-    Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
-
-    Write-Host "OSDCloud Complete" -ForegroundColor Green
+    
+    #Stop the startup Transcript.  OSDCloud will create its own
     $null = Stop-Transcript -ErrorAction Ignore
 }
 #endregion
 
 #region Specialize
-if ($WindowsPhase -eq 'Specialize') { $null = Stop-Transcript -ErrorAction Ignore }
+if ($WindowsPhase -eq 'Specialize') {
+    $null = Stop-Transcript -ErrorAction Ignore
+}
 #endregion
 
 #region AuditMode
-if ($WindowsPhase -eq 'AuditMode') { $null = Stop-Transcript -ErrorAction Ignore }
+if ($WindowsPhase -eq 'AuditMode') {
+    $null = Stop-Transcript -ErrorAction Ignore
+}
 #endregion
 
 #region OOBE
-if ($WindowsPhase -eq 'OOBE') { $null = Stop-Transcript -ErrorAction Ignore }
+if ($WindowsPhase -eq 'OOBE') {
+    #Load everything needed to run AutoPilot and Azure KeyVault
+    osdcloud-StartOOBE -Display -Language -DateTime -Autopilot -KeyVault -InstallWinGet -WinGetUpgrade -WinGetPwsh
+    $null = Stop-Transcript -ErrorAction Ignore
+}
 #endregion
 
 #region Windows
-if ($WindowsPhase -eq 'Windows') { $null = Stop-Transcript -ErrorAction Ignore }
+if ($WindowsPhase -eq 'Windows') {
+    #Load OSD and Azure stuff
+    $null = Stop-Transcript -ErrorAction Ignore
+}
 #endregion
